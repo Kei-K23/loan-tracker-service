a7f471f8d114e4ac3f7a7e2661a091c490258e4d
Kei-K23 <keiksl2301@gmail.com>
2025-02-26 23:14:26 +0630

    feat: add basic CRUD for user

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Setup Swagger
+  const documentConfig = new DocumentBuilder()
+    .setTitle('Loan Tracker Service')
+    .setVersion('0.1.0')
+    .setDescription(
+      'The Loan Tracker Service API allows users to apply for loans, manage repayments, and track their loan status',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, documentConfig);
+  SwaggerModule.setup('/swagger', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
